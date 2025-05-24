1d849fbd5ce1a773170c8cd8f4d98acd60584115
Teetaiwo <emmatee4real24@gmail.com>
2025-05-24 02:18:14 +0100

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import Logo from "./assets/Images/logo.png";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
 import Hero from "./Components/Hero";
 import HomeCards from "./Components/HomeCards";
 import JobListings from "./Components/JobListings";
 import NavBar from "./Components/NavBar";
+import ViewAllJobs from "./Components/ViewAllJobs";
+import HomePage from "./Pages/HomePage";
+import MainLayout from "./Layouts/MainLayout";
+import JobsPage from "./Pages/JobsPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <NavBar/>
-      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
-      <HomeCards/>
-      <JobListings/>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "../Components/NavBar"
+import Hero from "../Components/Hero"
+import HomeCards from "../Components/HomeCards"
+import JobListings from "../Components/JobListings"
+import ViewAllJobs from "../Components/ViewAllJobs"
+
+const HomePage = () => {
+  return (
+    <>
+      
+      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
