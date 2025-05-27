833188d05fe320acb1db2da7849c0fd0363bb14d
Teetaiwo <emmatee4real24@gmail.com>
2025-05-27 05:48:57 +0100

    jobs page done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
       
       <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
       <HomeCards />
-      <JobListings />
+      <JobListings isHome={true}/>
       <ViewAllJobs />
     </>
   );
--- src/Pages/JobsPage.jsx
+++ src/Pages/JobsPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import jobs from "../jobs.json"
+import JobListings from '../Components/JobListings'
 
 const JobsPage = () => {
+  
   return (
-    <div>JobsPage</div>
+    <>
+      <section className='bg-blue-50 px-4 py-6'>
+        <JobListings />
+      </section>
+    </>
   )
 }
 
